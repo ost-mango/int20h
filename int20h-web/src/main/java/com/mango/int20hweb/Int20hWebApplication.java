0803d2706349ca3707ad8bfd1140e8089bda223c
Andriy.Mishchuk <andriy.mishchuk@kyivstar.net>
2019-02-05 23:53:20 +0200

    initial commit

---
+++ int20h-web/src/main/java/com/mango/int20hweb/Int20hWebApplication.java
@@ -0,0 +1,14 @@
+package com.mango.int20hweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Int20hWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Int20hWebApplication.class, args);
+    }
+
+}
+
